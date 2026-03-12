daa11f38f2e787c8a3143c6786d1cb52ee0a8944
Zane Huang <193303856+huang-zane@users.noreply.github.com>
2026-03-12 11:47:38 +0800

    feat(ui): 增列說明文字

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="container">
-    <h1>國立臺北大學最新公告(法律學院系)</h1>
+    <h1>臺北大學法律學院系公告訊息</h1>
     <div style="margin-bottom: 10px;">
-        訂閱 RSS: <a href="/rss/lawsch.xml" target="_blank" class="rss-link">
-        院
-        </a>、<a href="/rss/lawdept.xml" target="_blank" class="rss-link">
-        系
-        </a>、<a href="/rss/law.xml" target="_blank" class="rss-link">
-        整合
-        </a>
+      <p>院網最新訊息（限20則），及系網最新訊息，依發布時間降序列出。</p>  
+      <p><strong>訂閱 RSS：</strong><a href="/rss/lawsch.xml" target="_blank" class="rss-link">院</a>、<a href="/rss/lawdept.xml" target="_blank" class="rss-link">系</a>、<a href="/rss/law.xml" target="_blank" class="rss-link">整合</a></p>
     </div>
 
     <div v-if="pending" class="loading">正在讀取訊息...</div>
